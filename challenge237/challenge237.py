3cfa643f028fb41fc6be9b571399c9fb701688c0
Zachary Glass <zac@signetcs.com>
2015-10-22 17:24:57 -0400

    works for one word only. will fix tomorrow

--- challenge237/challenge237.py
+++ challenge237/challenge237.py
@@ -1,6 +1,43 @@
 def main():
+	#open enable1.txt
+	dictionary = open('enable1.txt','r')
 	
-	input("Done!")
+	inputs = []
+	
+	#get paramaters
+	i = int(input("How many keyboards?"))
+	while i > 0:
+		inputs.append(input("Enter a keyboard..."))
+		i -= 1
 
+	#for each keyboard, loop over dictionary and add longest typable word to outputs 
+	for keyboard in inputs:
+		longestWord = ""
+
+		#loop over words
+		for word in dictionary:
+			word = word.strip()
+			skip = False
+
+			#loop over the letters in each word
+			for letter in word:
+				#if the word can't be typed, flag it for skipping
+				if letter not in keyboard:
+					
+					skip = True
+				
+			#if the word was found to be untypable, skip it. Otherwise, if it
+			#was longer than the current longestWord, set it as longestWord
+			if skip == True:
+				continue
+
+			if len(word) > len(longestWord):
+				longestWord = word
+		
+		#print result
+		print(keyboard, " = ", longestWord)
+			
+	#pause at end for viewing
+	input("Done!")
 
 main()
