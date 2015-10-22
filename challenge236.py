3c08390417fdcf7606211a8da8a493ff2574bb5b
Zachary Glass <zac@signetcs.com>
2015-10-22 12:14:49 -0400

    it works perfectly, though I want to give resetBag() a facelift

--- challenge236.py
+++ challenge236.py
@@ -1,13 +1,12 @@
 import random
 
-def resetBag(bag):
-	bag = {1:["O",True], 2:["I",True], 3:["S",True], 4:["Z",True], 5:["L",True], 6:["J",True], 7:["T",True]}
+def resetBag():
+	return {1:["O",True], 2:["I",True], 3:["S",True], 4:["Z",True], 5:["L",True], 6:["J",True], 7:["T",True]}
 
 def getPiece(bag):
 	switch = True
 	while switch:
 		index = random.randint(1,7)
-		
 		if bag[index][1] == True:
 			bag[index][1] = False
 			switch = False
@@ -15,8 +14,7 @@ def getPiece(bag):
 
 def main():
 	#initialize bag and output string
-	bag = {}
-	resetBag(bag)
+	bag = resetBag()
 	output = ""
 
 	#50 iterations
@@ -24,9 +22,10 @@ def main():
 	while i > 0:
 		
 		if ((i+6)%7) == 0: 
-			resetBag(bag)
+			bag = resetBag()
 		
 		output += getPiece(bag)
+		i -= 1
 
 	print(output)
 	input("Done!")
